04d285dc3b8aa9d719c59d0dac656d21355bd54b
therealraboffel <markus@wolf-net.info>
2026-05-29 21:50:19 +0200

    adds tag/value filter to problem.get call

--- src/routes/api.js
+++ src/routes/api.js
@@ -22,11 +22,20 @@ const getproblems = (zbxcli,param) => {
 
 function zbx_pb_to_prometheus(zbxcli,params) {
   return new Promise((resolve, reject) => {
+    let problemTag = params.problem_tag || ''
+    let problemValue = params.problem_value || ''
+    delete params.problem_tag
+    delete params.problem_value
+    let problemParams = {}
+    if (problemTag) {
+      problemParams.tags = [{tag: problemTag, value: problemValue, operator: 'equals'}]
+      problemParams.evaltype = 0
+    }
     meta_name="zabbix_problem"
     lines="# HELP "+meta_name+' problem zabbix\n# TYPE '+meta_name+' gauge'+'\n'
     output=lines
     
-    getproblems(zbxcli,params,{"output":"extend"})
+    getproblems(zbxcli,problemParams)
     .then(function(problems)  {
         let i=0
         for(i=0 ;i < problems.length;i++){
@@ -131,7 +140,12 @@ router.get('/metrics/prometheus', function(req, res, next) {
     params.item_tag = res.locals.config.zbx.filter.tag
     params.item_value = res.locals.config.zbx.filter.value || ''
   }
-  const result =  Promise.all([zbx_pb_to_prometheus(res.locals.zbxcon,{}), zbx_items_to_prometheus(res.locals.zbxcon, params)]).then(values => {
+  let pbParams = {}
+  if (res.locals.config.zbx.filter && res.locals.config.zbx.filter.tag) {
+    pbParams.problem_tag = res.locals.config.zbx.filter.tag
+    pbParams.problem_value = res.locals.config.zbx.filter.value || ''
+  }
+  const result =  Promise.all([zbx_pb_to_prometheus(res.locals.zbxcon, pbParams), zbx_items_to_prometheus(res.locals.zbxcon, params)]).then(values => {
 
     res.set('Content-Type', 'text/plain');
     for(i=0 ;i < values.length;i++){
